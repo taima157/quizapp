86406f70c82529216377f92ac5cad8dc6d479ea3
Gustavo <gustavotaima18@gmail.com>
2024-05-22 16:32:51 -0300

    ajustes da função de selecionar assunto para funcionar em todos os dispositivos

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ botaoTema.addEventListener("click", () => {
 
 verificarTema(body, botaoTema);
 
-const botoesAssunto = document.querySelectorAll(".assuntos button")
+const botoesAssunto = document.querySelectorAll(".assuntos button");
 
 botoesAssunto.forEach((botao) => {
-  botao.addEventListener("click", selecionarAssunto)
-})
+  botao.addEventListener("click", selecionarAssunto);
+});
 
 function selecionarAssunto(evento) {
-  const assunto = evento.target.innerText
+  const classeBotao = evento.target.className;
+  const assunto = document.querySelector(`.${classeBotao} span`).innerText;
 
-  localStorage.setItem("assunto", assunto)
-  window.location.href = "./pages/quiz/quiz.html"
+  localStorage.setItem("assunto", assunto);
+  window.location.href = "./pages/quiz/quiz.html";
 }
